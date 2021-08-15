ed5dc7399548c6b2a8db831adb0513aa7d11462a
Victor <victor.zorin.adci@gmail.com>
2021-08-15 02:33:59 -0700

    Add document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-import { AppProps } from 'next/dist/shared/lib/router/router';
-import '../styles/globals.css';
+import { AppProps } from "next/dist/shared/lib/router/router";
+import "../styles/globals.css";
+import React from "react";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Top</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
