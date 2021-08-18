2790434544af786c8b50f7505d8702d6fbce5308
Victor <victor.zorin.adci@gmail.com>
2021-08-18 08:43:11 -0700

    Add Tag, P

--- next-env.d.ts
+++ next-env.d.ts
@@ -2,11 +2,5 @@
 /// <reference types="next/types/global" />
 /// <reference types="next/image-types/global" />
 
-// svg
-declare module "*.svg" {
-    const content: any;
-    export default content;
-}
-
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Htag } from "../components";
+import { Button, Htag, P, Taq } from "../components";
 
 export default function Home(): JSX.Element {
   return (
@@ -13,6 +13,16 @@ export default function Home(): JSX.Element {
       <Button appearance="ghost" arrow="down">
         Button another
       </Button>
+
+      <P size='l'>Large</P>
+      <P size='m'>Medium</P>
+      <P size='s'>Small</P>
+
+      <Taq size='s'>Ghds</Taq>
+      <Taq size='m' color='red'>123</Taq>
+      <Taq size='s' color='green'>dddfwe</Taq>
+      <Taq color='primary'>FRds</Taq>
+
 
     </div>
   );
